6cadf1d304e490e8276c32e6782ded998834e4e3
henriquepercudeoliveira <henrique.percu@gmail.com>
2024-12-04 19:52:15 -0300

    Adding error handling middleware

--- apis/services/sales/route/sys/checkapi/checkapi.go
+++ apis/services/sales/route/sys/checkapi/checkapi.go
@@ -2,7 +2,9 @@ package checkapi
 
 import (
 	"context"
+	"github.com/HenriquePercu/go-service/app/api/errs"
 	"github.com/HenriquePercu/go-service/foundation/web"
+	"math/rand"
 	"net/http"
 )
 
@@ -15,6 +17,18 @@ func liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 }
 
 func readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	status := struct {
+		Status string
+	}{Status: "OK"}
+
+	return web.Respond(ctx, w, status, http.StatusOK)
+}
+
+func testError(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return errs.Newf(errs.FailedPrecondition, "this is an error")
+	}
+
 	status := struct {
 		Status string
 	}{Status: "OK"}
